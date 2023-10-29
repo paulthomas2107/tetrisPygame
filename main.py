ee0acb1a1c2ef65a8d3ba42b851cd5c83d20ff56
Paul Thomas <paulthomas2107@gmail.com>
2023-10-29 20:29:52 +0000

    More

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pygame, sys
+from grid import Grid
 
 pygame.init()
+dark_blue = (44, 44, 127)
 
 screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock()
 
+game_grid = Grid()
+game_grid.print_grid()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+    # Drawing
+    screen.fill(dark_blue)
+    game_grid.draw(screen)
     pygame.display.update()
     clock.tick(60)
-
