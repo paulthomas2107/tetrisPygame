eabcdaf353466e6f221c78918e20effae14ca181
Paul Thomas <paulthomas2107@gmail.com>
2023-10-30 17:00:20 +0000

    finish

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from grid import Grid
 from blocks import *
 import random
+import pygame
 
 
 class Game:
@@ -11,6 +12,10 @@ class Game:
         self.next_block = self.get_random_block()
         self.game_over = False
         self.score = 0
+        self.rotate_sound = pygame.mixer.Sound("sounds/rotate.ogg")
+        self.clear_sound = pygame.mixer.Sound("sounds/clear.ogg")
+        pygame.mixer.music.load("sounds/music.ogg")
+        pygame.mixer.music.play(-1)
 
     def update_score(self, lines_cleared, move_down_points):
         if lines_cleared == 1:
@@ -51,7 +56,9 @@ class Game:
         self.current_block = self.next_block
         self.next_block = self.get_random_block()
         rows_cleared = self.grid.clear_full_rows()
-        self.update_score(rows_cleared, 0)
+        if rows_cleared > 0:
+            self.clear_sound.play()
+            self.update_score(rows_cleared, 0)
         if not self.block_fits():
             self.game_over = True
 
@@ -73,6 +80,8 @@ class Game:
         self.current_block.rotate()
         if not self.block_inside() or self.block_fits() is False:
             self.current_block.undo_rotation()
+        else:
+            self.rotate_sound.play()
 
     def block_inside(self):
         tiles = self.current_block.get_cell_positions()
