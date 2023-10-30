4ff6433950f05cbc3e223490457ebbed4e1b15bd
Paul Thomas <paulthomas2107@gmail.com>
2023-10-30 09:35:09 +0000

    More

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from grid import Grid
+from blocks import *
 
 pygame.init()
 dark_blue = (44, 44, 127)
@@ -11,11 +12,7 @@ clock = pygame.time.Clock()
 
 game_grid = Grid()
 
-game_grid.grid[0][0] = 1
-game_grid.grid[3][5] = 4
-game_grid.grid[17][8] = 7
-
-game_grid.print_grid()
+block = LBlock()
 
 while True:
     for event in pygame.event.get():
@@ -25,5 +22,6 @@ while True:
     # Drawing
     screen.fill(dark_blue)
     game_grid.draw(screen)
+    block.draw(screen)
     pygame.display.update()
     clock.tick(60)
